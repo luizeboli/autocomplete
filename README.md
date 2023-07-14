08793157b42a0c3d78a60290e0359afe7d9ae479
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-07-14 11:41:55 -0300

    docs: add readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Autocomplete
+
+This is an Autocomplete component exercise written in React + Typescript.
+
+## Getting Started
+
+1. Clone or download this repository
+2. Install the dependencies
+```sh
+npm install
+```
+3. Start the web server
+```sh
+npm run dev
+```
+4. Open your browser at http://localhost:5173
+
+> We are using the fake API https://jsonplaceholder.typicode.com/, so this value is hardcoded in [/src/App.tsx](./src/App.tsx).
+
+> You can use other data sources but keep in mind the return type should be the same as declared in [/src/types/index.ts](./src/types/index.ts) (User type)
+
+## Running tests
+
+We have some tests written using Vitest.
+
+For a single run execute the command
+```sh
+npm run test
+```
+
+For the watch mode execute the command
+```sh
+npm run test:watch
+```
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { Autocomplete } from "./components/Autocomplete";
 import { User } from "./types";
 import "./app.css";
 
+// We are hardcoding the API URL here, but in a real world scenario
+// we would probably have a service helper to handle this
+// With the value coming from an environment variable
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
 function App() {
   const [, setSelected] = useState<User>();
 
@@ -15,12 +20,7 @@ function App() {
           label="Search users"
           placeholder="Start typing to search for users"
           filterOptions={async (searchTerm) => {
-            // We are hardcoding the API URL here, but in a real world scenario
-            // we would probably have a service helper to handle this
-            // With the value coming from an environment variable
-            const response = await fetch(
-              "https://jsonplaceholder.typicode.com/users",
-            );
+            const response = await fetch(`${BASE_URL}/users`);
             const data = (await response.json()) as User[];
 
             // In a real world scenario the API would probably search considering a "like" statement
